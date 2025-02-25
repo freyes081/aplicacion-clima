79972f7c58142e17e35553328881d27dee35df69
Frank Reyes <freyes081@gmail.com>
2025-02-25 13:59:15 -0500

    Versión inicial de la Aplicación de Clima

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+from clima import get_weather
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/weather', methods=['POST'])
+def weather():
+    city = request.form['city']
+    weather_data = get_weather(city)
+    if weather_data:
+        return render_template('weather.html', weather=weather_data, city=city)
+    else:
+        return render_template('index.html', error="No se encontró información para la ciudad ingresada")
+   
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
